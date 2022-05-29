1eaa561f8446f80bdbec9e326edd578a97c366cc
DucAnh <ducanh2k2@gmail.com>
2022-05-29 15:16:19 +0700

    Front-End

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
-var modal = document.getElementById('style-modal');
+var style_modal = document.getElementById('style-modal');
+var modal = style_modal.querySelector('.modal');
 
 var btnBuyTickets = document.getElementsByClassName('js-buy-ticket');
 for(let i = 0; i < btnBuyTickets.length; i++) {
     btnBuyTickets[i].onclick = () => {
-        modal.style.display = 'block';
+        style_modal.style.display = 'block';
     }
 }
 
-modal.querySelector('.modal .header .button-close').onclick = () => {
-    modal.style.display = 'none';
+style_modal.querySelector('.modal .header .button-close').onclick = () => {
+    style_modal.style.display = 'none';
 }
-modal.querySelector('.modal .footer .close').onclick = () => {
-    modal.style.display = 'none';
+style_modal.querySelector('.modal .footer .close').onclick = () => {
+    style_modal.style.display = 'none';
+}
+style_modal.onclick = function(e) {
+    if(e.target == style_modal) {
+        style_modal.style.display = 'none';
+    }    
 }
